b1b500cc156721a091b5c0b7c467526f9edc303d
TobiraKhoa Senju <kiraisme182003@gmail.com>
2024-04-20 15:19:41 +0700

    Rut gon

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import pygame
-from game import Game
-from main_menu import MainMenu
+import sys
+from game import *
+class MainMenu:
+    def __init__(self, screen, game):
+        self.screen = screen
+        self.game = game
+        self.WIDTH = screen.get_width()
+        self.HEIGHT = screen.get_height()
+        self.BG = pygame.image.load(r'images/background.jpg')
+        self.play_button = pygame.image.load(r'images/playbutton.PNG')
+        self.play_button = pygame.transform.scale(self.play_button, (300, 230))
+        self.play_rect = self.play_button.get_rect(center=(500, 300))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        running = True
+        while running:
+            self.screen.blit(self.BG, (0, 0))
+            self.screen.blit(self.play_button, self.play_rect)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    if self.play_rect.collidepoint(x, y):
+                        running = False
+                        self.game.menu.run()  # Start the game after selecting a level from the menu
+            pygame.display.flip()
+            self.clock.tick(60)
+
 
 def main():
     pygame.init()
     game = Game()
-    main_menu = MainMenu(pygame.display.set_mode((1000, 700)), game)
+    screen = pygame.display.set_mode((1000, 700))
+    main_menu = MainMenu(screen, game)
     main_menu.run()
-    game.run()  # Bắt đầu chơi game sau khi chọn level từ menu
+    game.run()
     pygame.quit()
 
 if __name__ == "__main__":
